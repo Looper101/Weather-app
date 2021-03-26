0b87cd284c3ef8767f281d813a0c55db403bc9df
Looper101 <hemkeys@gmail.com>
2021-03-26 16:32:13 +0100

    refactoring

---
+++ lib/dataproviders/cityid_provider.dart
@@ -0,0 +1,51 @@
+import 'package:http/http.dart' as http;
+import 'package:location/location.dart';
+
+// class WeatherApiREduntant {
+//   // final apiKey = 'dd2802091dc6f5d51f064d6ce8fb8441';
+
+//   ///Fetch weather data by using the typed in city name--WeatherProvider
+//   Future<String> getRawData(String city) async {
+//     final String baseUrl =
+//         'https://api.openweathermap.org/data/2.5/weather?q=$city&units=imperial&appid=$apiKey';
+
+//     http.Response response = await http.get(baseUrl);
+//     return response.body;
+//   }
+
+//   ///Fetch weather data from serve using user location--WeatherProvider
+//   Future<String> weatherDataForLocation(LocationData loc) async {
+//     String baseUrl =
+//         "https://api.openweathermap.org/data/2.5/weather?lat=${loc.latitude}&lon=${loc.longitude}&appid=$apiKey";
+
+//     http.Response response = await http.get(baseUrl);
+
+//     return response.body;
+//   }
+// }
+
+class CityIdApi {
+  // final String apiKey = 'dd2802091dc6f5d51f064d6ce8fb8441';
+
+//Fetch weather data by using typed city query
+  Future<String> getCityIdByQuery(String cityName) async {
+    String url =
+        "https://www.metaweather.com/api/location/search/?query=$cityName";
+
+    http.Response response = await http.get(url);
+    return response.body;
+  }
+
+  //Fetch weather data using user current coordinate(location)
+  Future<String> getCityIdByLocation(LocationData loc) async {
+    String url =
+        "https://www.metaweather.com/api/location/search/?lattlong=${loc.longitude},${loc.latitude}";
+
+    http.Response response = await http.get(url);
+
+    return response.body;
+  }
+
+  //TODO: Add getWeatherDataByQuery
+  //TODO:Add getWeatherDatabyLocation
+}
---
+++ lib/repositories/cityId_repository.dart
@@ -0,0 +1,24 @@
+import 'package:location/location.dart';
+import 'package:songlyrics/models/city_id.dart';
+
+import '../dataproviders/cityid_provider.dart';
+
+class CityIdRepository {
+  CityIdApi _cityIdApi = CityIdApi();
+
+  /// Fetch city-id data by the user typed city name ex-. London
+  Future<City> getWeatherData(String query) async {
+    String rawResponse = await _cityIdApi.getCityIdByQuery(query);
+    City city = City.fromJson(rawResponse);
+
+    return city;
+  }
+
+  ///Fetch city-id data by using user current location-i.e Co-ordinates
+  Future<City> fetchWeatherByLocation(LocationData location) async {
+    String rawResponse = await _cityIdApi.getCityIdByLocation(location);
+
+    City city = City.fromJson(rawResponse);
+    return city;
+  }
+}
