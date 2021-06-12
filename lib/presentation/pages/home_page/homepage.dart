3ef0899b27f23550104ec4adb91a868506885697
Devlonoah <hemkeys@gmail.com>
2021-06-12 21:20:04 +0100

    few refactoring

--- lib/presentation/pages/home_page/homepage.dart
+++ lib/presentation/pages/home_page/homepage.dart
@@ -6,7 +6,7 @@ import 'package:songlyrics/presentation/pages/home_page/widgets/weather_details.
 import 'package:songlyrics/presentation/pages/home_page/widgets/weather_details_shimmer_effect.dart';
 import 'package:songlyrics/theme/mediaquery.dart';
 
-import 'widgets/build_appbar.dart';
+import 'widgets/widgets.dart';
 
 class HomePage extends StatefulWidget {
   static String id = 'homepage';
@@ -20,8 +20,10 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     DeviceSize deviceOrientation = DeviceSize();
     deviceOrientation.init(context);
+
+    var _bloc = BlocProvider.of<WeatherBloc>(context);
     return Scaffold(
-      drawer: _buildDrawer(context),
+      drawer: buildDrawer(context),
       appBar: buildAppBar(context),
       body: Container(
         height: DeviceSize.height,
@@ -60,115 +62,17 @@ class _HomePageState extends State<HomePage> {
             }
 
             if (state is WeatherLoadCityException) {
-              return Stack(
-                children: [
-                  Positioned(
-                    top: DeviceSize.height * 0.01,
-                    left: DeviceSize.width * 0.05,
-                    child: GestureDetector(
-                      onTap: () => BlocProvider.of<WeatherBloc>(context)
-                          .add(FetchWeatherByLocation()),
-                      child: Container(
-                        // height: DeviceSize.height * 0.09,
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.all(DeviceSize.longestSide * 0.02),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.white,
-                        ),
-                        child: RotatedBox(
-                          quarterTurns: 2,
-                          child: Icon(
-                            Icons.navigation_sharp,
-                            size: DeviceSize.longestSide * 0.03,
-                            color: Theme.of(context).errorColor,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: DeviceSize.height * 0.03,
-                    left: DeviceSize.width * 0.16,
-                    child: Container(
-                      height: DeviceSize.height * 0.03,
-                      padding: EdgeInsets.symmetric(
-                          horizontal: DeviceSize.width * 0.02),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(10),
-                            bottomRight: Radius.circular(10),
-                          )),
-                      child: Text(
-                        'Fetch Weather for my Location',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontFamily: 'Bold'),
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Text(
-                      'City can\'t be found !',
-                      style: TextStyle(
-                          fontFamily: 'Bold',
-                          fontSize: DeviceSize.longestSide * 0.025),
-                    ),
-                  ),
-                ],
-              );
+              return ShowOnError(bloc: _bloc);
             }
 
-            return _buildErrorMessage();
+            return _buildErrorMessage(_bloc);
           },
         ),
       ),
     );
   }
 
-  Widget _buildDrawer(BuildContext context) {
-    return Drawer(
-      child: Container(
-        color: Theme.of(context).buttonColor,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              DrawerHeader(
-                padding: EdgeInsets.only(top: 20),
-                child: Text('P-O-I-N-Q'),
-              ),
-              SizedBox(),
-              // ignore: deprecated_member_use
-              FlatButton(
-                color: Colors.blueGrey[200],
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AboutDialog(
-                          applicationName: 'Poinq',
-                          applicationVersion: 'version 1.0.0',
-                          //TODO: add app icon here
-                          applicationIcon: Text('To e added soon'),
-                        );
-                      });
-                },
-                child: Text('About Developer',
-                    style: Theme.of(context).textTheme.headline5.copyWith(
-                        fontSize: DeviceSize.longestSide * 0.03,
-                        fontWeight: FontWeight.w100)),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  _buildErrorMessage() {
+  _buildErrorMessage(WeatherBloc bloc) {
     return Stack(
       children: [
         Positioned(
@@ -238,10 +142,76 @@ class _HomePageState extends State<HomePage> {
                   'Retry',
                   style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
-                onPressed: () => BlocProvider.of<WeatherBloc>(context)
-                    .add(FetchWeatherByLocation()),
+                onPressed: () => bloc.add(FetchWeatherByLocation()),
               ),
             ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ShowOnError extends StatelessWidget {
+  const ShowOnError({
+    Key key,
+    @required WeatherBloc bloc,
+  })  : _bloc = bloc,
+        super(key: key);
+
+  final WeatherBloc _bloc;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          top: DeviceSize.height * 0.01,
+          left: DeviceSize.width * 0.05,
+          child: GestureDetector(
+            onTap: () => _bloc.add(FetchWeatherByLocation()),
+            child: Container(
+              // height: DeviceSize.height * 0.09,
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(DeviceSize.longestSide * 0.02),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+              child: RotatedBox(
+                quarterTurns: 2,
+                child: Icon(
+                  Icons.navigation_sharp,
+                  size: DeviceSize.longestSide * 0.03,
+                  color: Theme.of(context).errorColor,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: DeviceSize.height * 0.03,
+          left: DeviceSize.width * 0.16,
+          child: Container(
+            height: DeviceSize.height * 0.03,
+            padding: EdgeInsets.symmetric(horizontal: DeviceSize.width * 0.02),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                )),
+            child: Text(
+              'Fetch Weather for my Location',
+              style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Bold'),
+            ),
+          ),
+        ),
+        Center(
+          child: Text(
+            'City can\'t be found !',
+            style: TextStyle(
+                fontFamily: 'Bold', fontSize: DeviceSize.longestSide * 0.025),
           ),
         ),
       ],
